49790d7504d1df12ad19b21fe1f548c3e96094c4
Vaibhav Nagpal <vaibhav@griffy.app>
2024-10-29 18:45:33 +0530

    fix: changed market data model

--- marketData/models.go
+++ marketData/models.go
@@ -3,9 +3,10 @@ package marketData
 import "time"
 
 type MarketData struct {
-	ID        string    `json:"id"`
-	Title     string    `json:"title"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Expiry    time.Time `json:"expiry"`
+	ID             string    `json:"id"`
+	Title          string    `json:"title"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	Expiry         time.Time `json:"expiry"`
+	SettlementDate time.Time `json:"settlement_date"`
 }
